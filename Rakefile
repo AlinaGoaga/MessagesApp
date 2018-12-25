e0558a7dca8754f762fb771df9af9f72db18f86d
Alina Goaga <goaga.alina@gmail.com>
2018-12-26 03:04:49 +0700

    Able to run rake from the console - need to update messages and not have to pass the RACK_ENV value in the command

---
+++ Rakefile
@@ -0,0 +1,11 @@
+require './config/data_mapper'
+
+desc "auto_upgrade"
+task :auto_upgrade do
+  DataMapper.auto_upgrade!
+end
+
+desc "auto_migrate"
+task :auto_migrate do
+  DataMapper.auto_migrate!
+end
--- config/data_mapper.rb
+++ config/data_mapper.rb
@@ -1,7 +1,4 @@
-
 require './lib/message'
 
 DataMapper.setup(:default, "postgres://localhost/messages_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
-DataMapper.auto_upgrade! if ENV['RACK_ENV'] == 'development'
-DataMapper.auto_migrate! if ENV['RACK_ENV'] == 'test'
